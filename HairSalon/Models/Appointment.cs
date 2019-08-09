837f9dccc829d52ca8f4a67c5434921cd5a247ef
Na Hyung Choi <schoinh@gmail.com>
2019-08-09 16:06:09 -0700

    remove Time property

--- HairSalon/Models/Appointment.cs
+++ HairSalon/Models/Appointment.cs
@@ -8,7 +8,6 @@ namespace HairSalon.Models
         public int StylistId { get; set; }
         public int ClientId { get; set; }
         public DateTime Date { get; set; }
-        public string Time { get; set; }
         public string Notes { get; set; }
         public virtual Client Client { get; set; }
         public virtual Stylist Stylist { get; set; }
