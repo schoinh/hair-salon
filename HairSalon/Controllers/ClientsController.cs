3536ad68ecae6d409a8cf3c01f40ea56f37e56c8
Na Hyung Choi <schoinh@gmail.com>
2019-08-09 12:49:39 -0700

    combine first and last name fields to 1 name field

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -24,7 +24,7 @@ namespace HairSalon.Controllers
 
         public ActionResult Create()
         {
-            ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "FirstName");
+            ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
             return View();
         }
 
